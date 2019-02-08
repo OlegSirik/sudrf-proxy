3591edb1927d203f342576d7739ef29282075bbf
Oleg <Oleg.Sirik@mail.ru>
2019-02-08 09:46:38 +0300

    add data dump sql folder

---
+++ sql/dict_region.sql
@@ -0,0 +1,108 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'WIN1251';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: dict_regions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO dict_regions VALUES ('28', 'Амурская область', NULL, NULL, 28);
+INSERT INTO dict_regions VALUES ('29', 'Архангельская область', NULL, NULL, 29);
+INSERT INTO dict_regions VALUES ('30', 'Астраханская область', NULL, NULL, 30);
+INSERT INTO dict_regions VALUES ('31', 'Белгородская область', NULL, NULL, 31);
+INSERT INTO dict_regions VALUES ('32', 'Брянская область', NULL, NULL, 32);
+INSERT INTO dict_regions VALUES ('33', 'Владимирская область', NULL, NULL, 33);
+INSERT INTO dict_regions VALUES ('34', 'Волгоградская область', NULL, NULL, 34);
+INSERT INTO dict_regions VALUES ('35', 'Вологодская область', NULL, NULL, 35);
+INSERT INTO dict_regions VALUES ('36', 'Воронежская область', NULL, NULL, 36);
+INSERT INTO dict_regions VALUES ('77', 'Город Москва', NULL, NULL, 77);
+INSERT INTO dict_regions VALUES ('78', 'Город Санкт-Петербург', NULL, NULL, 78);
+INSERT INTO dict_regions VALUES ('92', 'Город Севастополь', NULL, NULL, 92);
+INSERT INTO dict_regions VALUES ('79', 'Еврейская автономная область', NULL, NULL, 79);
+INSERT INTO dict_regions VALUES ('75', 'Забайкальский край', NULL, NULL, 75);
+INSERT INTO dict_regions VALUES ('37', 'Ивановская область', NULL, NULL, 37);
+INSERT INTO dict_regions VALUES ('38', 'Иркутская область', NULL, NULL, 38);
+INSERT INTO dict_regions VALUES ('07', 'Кабардино-Балкарская Республика', NULL, NULL, 7);
+INSERT INTO dict_regions VALUES ('39', 'Калининградская область', NULL, NULL, 39);
+INSERT INTO dict_regions VALUES ('40', 'Калужская область', NULL, NULL, 40);
+INSERT INTO dict_regions VALUES ('41', 'Камчатский край', NULL, NULL, 41);
+INSERT INTO dict_regions VALUES ('09', 'Карачаево-Черкесская Республика', NULL, NULL, 9);
+INSERT INTO dict_regions VALUES ('42', 'Кемеровская область', NULL, NULL, 42);
+INSERT INTO dict_regions VALUES ('43', 'Кировская область', NULL, NULL, 43);
+INSERT INTO dict_regions VALUES ('22', 'Алтайский край', '2018-12-05 17:21:12.541+03', NULL, 22);
+INSERT INTO dict_regions VALUES ('83', 'Ненецкий автономный округ', NULL, NULL, 83);
+INSERT INTO dict_regions VALUES ('16', 'Республика Татарстан', NULL, NULL, 16);
+INSERT INTO dict_regions VALUES ('44', 'Костромская область', NULL, NULL, 44);
+INSERT INTO dict_regions VALUES ('23', 'Краснодарский край', NULL, NULL, 23);
+INSERT INTO dict_regions VALUES ('24', 'Красноярский край', NULL, NULL, 24);
+INSERT INTO dict_regions VALUES ('45', 'Курганская область', NULL, NULL, 45);
+INSERT INTO dict_regions VALUES ('46', 'Курская область', NULL, NULL, 46);
+INSERT INTO dict_regions VALUES ('47', 'Ленинградская область', NULL, NULL, 47);
+INSERT INTO dict_regions VALUES ('48', 'Липецкая область', NULL, NULL, 48);
+INSERT INTO dict_regions VALUES ('49', 'Магаданская область', NULL, NULL, 49);
+INSERT INTO dict_regions VALUES ('50', 'Московская область', NULL, NULL, 50);
+INSERT INTO dict_regions VALUES ('51', 'Мурманская область', NULL, NULL, 51);
+INSERT INTO dict_regions VALUES ('52', 'Нижегородская область', NULL, NULL, 52);
+INSERT INTO dict_regions VALUES ('53', 'Новгородская область', NULL, NULL, 53);
+INSERT INTO dict_regions VALUES ('54', 'Новосибирская область', NULL, NULL, 54);
+INSERT INTO dict_regions VALUES ('55', 'Омская область', NULL, NULL, 55);
+INSERT INTO dict_regions VALUES ('56', 'Оренбургская область', NULL, NULL, 56);
+INSERT INTO dict_regions VALUES ('57', 'Орловская область', NULL, NULL, 57);
+INSERT INTO dict_regions VALUES ('58', 'Пензенская область', NULL, NULL, 58);
+INSERT INTO dict_regions VALUES ('59', 'Пермский край', NULL, NULL, 59);
+INSERT INTO dict_regions VALUES ('25', 'Приморский край', NULL, NULL, 25);
+INSERT INTO dict_regions VALUES ('60', 'Псковская область', NULL, NULL, 60);
+INSERT INTO dict_regions VALUES ('01', 'Республика Адыгея', NULL, NULL, 1);
+INSERT INTO dict_regions VALUES ('02', 'Республика Алтай', NULL, NULL, 2);
+INSERT INTO dict_regions VALUES ('03', 'Республика Башкортостан', NULL, NULL, 3);
+INSERT INTO dict_regions VALUES ('04', 'Республика Бурятия', NULL, NULL, 4);
+INSERT INTO dict_regions VALUES ('05', 'Республика Дагестан', NULL, NULL, 5);
+INSERT INTO dict_regions VALUES ('06', 'Республика Ингушетия', NULL, NULL, 6);
+INSERT INTO dict_regions VALUES ('08', 'Республика Калмыкия', NULL, NULL, 8);
+INSERT INTO dict_regions VALUES ('10', 'Республика Карелия', NULL, NULL, 10);
+INSERT INTO dict_regions VALUES ('11', 'Республика Коми', NULL, NULL, 11);
+INSERT INTO dict_regions VALUES ('91', 'Республика Крым', NULL, NULL, 91);
+INSERT INTO dict_regions VALUES ('12', 'Республика Марий Эл', NULL, NULL, 12);
+INSERT INTO dict_regions VALUES ('13', 'Республика Мордовия', NULL, NULL, 13);
+INSERT INTO dict_regions VALUES ('14', 'Республика Саха (Якутия)', NULL, NULL, 14);
+INSERT INTO dict_regions VALUES ('15', 'Республика Северная Осетия-Алания', NULL, NULL, 15);
+INSERT INTO dict_regions VALUES ('17', 'Республика Тыва', NULL, NULL, 17);
+INSERT INTO dict_regions VALUES ('19', 'Республика Хакасия', NULL, NULL, 19);
+INSERT INTO dict_regions VALUES ('61', 'Ростовская область', NULL, NULL, 61);
+INSERT INTO dict_regions VALUES ('62', 'Рязанская область', NULL, NULL, 62);
+INSERT INTO dict_regions VALUES ('63', 'Самарская область', NULL, NULL, 63);
+INSERT INTO dict_regions VALUES ('64', 'Саратовская область', NULL, NULL, 64);
+INSERT INTO dict_regions VALUES ('65', 'Сахалинская область', NULL, NULL, 65);
+INSERT INTO dict_regions VALUES ('66', 'Свердловская область', NULL, NULL, 66);
+INSERT INTO dict_regions VALUES ('67', 'Смоленская область', NULL, NULL, 67);
+INSERT INTO dict_regions VALUES ('26', 'Ставропольский край', NULL, NULL, 26);
+INSERT INTO dict_regions VALUES ('68', 'Тамбовская область', NULL, NULL, 68);
+INSERT INTO dict_regions VALUES ('69', 'Тверская область', NULL, NULL, 69);
+INSERT INTO dict_regions VALUES ('70', 'Томская область', NULL, NULL, 70);
+INSERT INTO dict_regions VALUES ('71', 'Тульская область', NULL, NULL, 71);
+INSERT INTO dict_regions VALUES ('72', 'Тюменская область', NULL, NULL, 72);
+INSERT INTO dict_regions VALUES ('18', 'Удмуртская Республика', NULL, NULL, 18);
+INSERT INTO dict_regions VALUES ('73', 'Ульяновская область', NULL, NULL, 73);
+INSERT INTO dict_regions VALUES ('27', 'Хабаровский край', NULL, NULL, 27);
+INSERT INTO dict_regions VALUES ('86', 'Ханты-Мансийский автономный округ - Югра (Тюменская область)', NULL, NULL, 86);
+INSERT INTO dict_regions VALUES ('74', 'Челябинская область', NULL, NULL, 74);
+INSERT INTO dict_regions VALUES ('20', 'Чеченская Республика', NULL, NULL, 20);
+INSERT INTO dict_regions VALUES ('21', 'Чувашская Республика - Чувашия', NULL, NULL, 21);
+INSERT INTO dict_regions VALUES ('87', 'Чукотский автономный округ', NULL, NULL, 87);
+INSERT INTO dict_regions VALUES ('89', 'Ямало-Ненецкий автономный округ', NULL, NULL, 89);
+INSERT INTO dict_regions VALUES ('76', 'Ярославская область', NULL, NULL, 76);
+INSERT INTO dict_regions VALUES ('95', 'Территории за пределами РФ', NULL, NULL, 95);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
